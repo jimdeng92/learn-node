0cf100bb626a10a24a8555470965cabb24c0202d
JimDeng92 <jimdeng92@gmail.com>
2020-11-25 16:39:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# learn-node
+
+- blog-1 使用原生 node
+
+
+nodemon 监测文件变化
+
+cross-env 设置环境变量
---
+++ blog-1/app.js
@@ -0,0 +1,13 @@
+const serverHandle = (req, res) => {
+  res.setHeader('Content-type', 'application/json')
+
+  const resData = {
+    name: 'jimdeng',
+    site: 'imooc',
+    env: process.env.NODE_ENV
+  }
+
+  res.end(JSON.stringify(resData))
+}
+
+module.exports = serverHandle
