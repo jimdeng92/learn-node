0b5bcd4c187eb76f6cb879c7efc2ce5d1eea3d66
JimDeng92 <jimdeng92@gmail.com>
2020-12-02 15:20:48 +0800

    express 处理路由

--- blog-express/app.js
+++ blog-express/app.js
@@ -1,37 +1,46 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+// 解析 cookie ，在路由中使用 req.cookies 访问
 var cookieParser = require('cookie-parser');
+// 记录日志
 var logger = require('morgan');
 
+// 路由
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const blogRouter = require('./routes/blog')
+const userRouter = require('./routes/user')
 
+// 初始化
 var app = express();
 
-// view engine setup
+// 视图引擎设置
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json()); // 处理 post (application/json)
+app.use(express.urlencoded({ extended: false })); // 处理 post (x-www-form-urlencoded)
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'))); // 静态文件
 
+// 注册路由
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/blog', blogRouter)
+app.use('/api/user', userRouter)
 
-// catch 404 and forward to error handler
+// 捕获404并转发到错误处理程序
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// 错误处理程序
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'dev' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
